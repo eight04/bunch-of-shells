b62d9d5a8d2f14d939eff316d9755d12bcf1531a
eight <eight04@gmail.com>
2015-05-07 10:11:25 +0800

    Add comment

--- Run Me!.bat
+++ Run Me!.bat
@@ -1,45 +1,48 @@
+:: This shell will execute the file passed in.
+:: Usage:
+::   runme "File"
+
 @echo off
 CD /D %~dp1
-REM echo %~dp1
-REM pause
-if %~x1 == .py goto %~x1
-if %~x1 == .pyw goto %~x1
-if %~x1 == .c goto %~x1
-if %~x1 == .cpp goto %~x1
-if %~x1 == .less goto %~x1
-if %~x1 == .js goto %~x1
-REM START /b CALL "%~nx1"
+FOR %%i IN (.py .pyw .c .cpp .less .js) DO (
+	IF %~x1 EQU %i GOTO %~x1
+)
 cmd /c start "" /d"%~dp1" "%~nx1"
-goto exit
-
+GOTO exit
 
 :.py
 :.pyw
-	REM chcp 950
+	:: Run python script
 	py "%~n1%~x1"
 	GOTO end
 	
 	
 :.c
+	:: Compile C
 	gcc -lm -O2 -pipe -DONLINE_JUDGE -o %~n1.exe %~n1%~x1
 	if errorlevel 1 goto end
 	echo Compile finished..
-	echo.
-	%~n1
-	goto end
+	GOTO execute
 
 :.cpp
+	:: Compile CPP
 	g++ -lm -O2 -pipe -DONLINE_JUDGE -o %~n1.exe %~n1%~x1
 	if errorlevel 1 goto end
-	echo Compile finished.
+	echo Compile finished..
+	GOTO execute
+	
+:execute
+	:: Execute
 	%~n1
 	goto end
 	
 :.less
+	:: Compile Less
 	lessc %~n1%~x1
 	goto end
 	
 :.js
+	:: Run node script
 	set "NODE_PATH=C:\Users\Owner\AppData\Roaming\npm\node_modules"
 	node %~n1%~x1
 	goto end
