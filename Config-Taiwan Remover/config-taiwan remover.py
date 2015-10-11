97c51f259fab0ce61f948ca0008ba4d52c6da379
eight <eight04@gmail.com>
2015-10-11 14:27:45 +0800

    Rename to *.py. Fix Permission error

--- Config-Taiwan Remover/config-taiwan remover.py
+++ Config-Taiwan Remover/config-taiwan remover.py
@@ -1,15 +1,23 @@
 #! python3
 
-import time, os
+import time, os, threading
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+dir = r"C:\Users\Owner\AppData\Local\Ankama\Wakfu\game"
+filename = "config-taiwan.properties"
+
+def remove():
+	try:
+		os.remove(os.path.join(dir, filename))
+	except FileNotFoundError:
+		pass
+	except PermissionError:
+		threading.Timer(1, remove).start()
+
 def check(event):
-	if os.path.basename(event.src_path) == "config-taiwan.properties":
-		try:
-			os.remove(event.src_path)
-		except FileNotFoundError:
-			pass
+	if os.path.basename(event.src_path) == filename:
+		remove()
 
 class EventHandler(FileSystemEventHandler):
 	def on_created(self, event):
@@ -20,8 +28,10 @@ class EventHandler(FileSystemEventHandler):
 
 event_handler = EventHandler()
 observer = Observer()
-observer.schedule(event_handler, path=r"C:\Users\Owner\AppData\Local\Ankama\Wakfu\game")
+observer.schedule(event_handler, path=dir)
 observer.start()
+
+remove()
 
 try:
 	while True:
